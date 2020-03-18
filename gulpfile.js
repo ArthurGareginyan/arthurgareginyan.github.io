d2ac319f503ab46aeb5e6bc784037db3abe7d968
Arthur Gareginyan <arthurgareginyan@gmail.com>
2020-03-19 00:45:11 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@
         concat    = require('gulp-concat'),
         uglify    = require('gulp-uglify'),
         babel     = require('gulp-babel'),
+        htmlmin   = require('gulp-htmlmin'),
 
         // Directory locations
         path = {
@@ -20,6 +21,28 @@
     (async (done) => {
         await del(['**/.DS_Store'], done);
     })();
+
+    function html_optimize () {
+        let html_files = [
+            `${path.build}/**/*.html`
+        ];
+        let htmlmin_opts = {
+            html5: true,                         // Parse input according to HTML5 specifications
+            //preserveLineBreaks: true,            // Always collapse to 1 line break (never remove it entirely) when whitespace between tags include a line break. Must be used in conjunction with collapseWhitespace=true
+            //collapseWhitespace: true,            // Collapse white space that contributes to text nodes in a document tree
+            //collapseBooleanAttributes: true,     // Omit attribute values from boolean attributes
+            //removeComments: true,                // Strip HTML comments
+            //removeEmptyElements: true,           // Remove all elements with empty contents
+            //removeEmptyAttributes: true,         // Remove all attributes with whitespace-only values
+            //removeScriptTypeAttributes: true,    // Remove type="text/javascript" from script tags. Other type attribute values are left intact
+            //removeStyleLinkTypeAttributes: true, // Remove type="text/css" from style and link tags. Other type attribute values are left intact
+            //minifyJS: true,                      // Minify JavaScript in script elements and event attributes (uses UglifyJS)
+            //minifyCSS: true                      // Minify CSS in style elements and style attributes (uses clean-css)
+        };
+        return src(html_files)
+            .pipe(htmlmin(htmlmin_opts))
+            .pipe(dest(`${path.build}/`));
+    }
 
     function javascript_bundle () {
         let js_files = [
@@ -41,6 +64,7 @@
         done();
     }
 
+    exports.html = html_optimize;
     exports.js = javascript_bundle;
     exports.default = default_function;
 
