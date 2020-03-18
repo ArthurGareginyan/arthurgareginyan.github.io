b7c903efe6c19705e190fa28b8ff61e75fcf5fa0
Arthur Gareginyan <arthurgareginyan@gmail.com>
2020-03-19 00:38:34 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,11 @@
     const
 
         // Modules
-        { parallel } = require('gulp'),
+        { src, dest, series, parallel } = require('gulp'),
         del = require('del'),
+        concat    = require('gulp-concat'),
+        uglify    = require('gulp-uglify'),
+        babel     = require('gulp-babel'),
 
         // Directory locations
         path = {
@@ -18,6 +21,19 @@
         await del(['**/.DS_Store'], done);
     })();
 
+    function javascript_bundle () {
+        let js_files = [
+            `${path.source}/scripts/*`
+        ];
+        return src(js_files)
+            .pipe(babel({
+                presets: ['@babel/env']
+            }))
+            .pipe(concat('scripts.js'))
+            .pipe(uglify())
+            .pipe(dest(`${path.build}/scripts/`));
+    }
+
     function default_function (done) {
 
         // Code here
@@ -25,6 +41,7 @@
         done();
     }
 
+    exports.js = javascript_bundle;
     exports.default = default_function;
 
 })();
